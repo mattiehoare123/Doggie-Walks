274bd7b76d3102f9a17a8764220d0703d00688e9
matthoare <23655704@edgehill.ac.uk>
2020-04-10 11:46:03 +0100

    Hero component styled

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const HeroSection = styled.div`
+const StyledHero = styled.section`
+width: 100%;
+height: 15vh;
+background-size: cover;
+background-repeat: no-repeat;
+background-size: 100% 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+poisiton: relative;
 
-`
-const HeroImg = styled.img`
-
+&::after {
+  content: '';
+  display: block;
+  position: absolute;
+  background: ${(props) => props.theme.colors.black};
+  opacity: 0.5;
+  width: 100%;
+  height: 15vh;
+  top: 0;
+  left: 0;
+}
 `
 const HeroTitle = styled.h1`
-
+width: 190px;
+font-size: 2rem;
+text-align: center;
+color: ${(props) => props.theme.colors.white};
+color: ${(props) => props.theme.weights.semi};
+z-index: 1;
 `
 
 export const Hero = (props) => {
 
   return (
-    <HeroSection>
-      <HeroImg src={props.image}  alt=""/>
+    <StyledHero style={{backgroundImage: `url(${props.image})` }}>
       <HeroTitle>{props.title}</HeroTitle>
-    </HeroSection>
+    </StyledHero>
   )
 }
